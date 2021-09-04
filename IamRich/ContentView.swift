5ba1af754ba4133f7f1aea7d688f856902248b17
carlosp <carlosmpr0508@gmail.com>
2021-09-04 17:33:27 -0400

    Initial Commit

---
+++ IamRich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IamRich
+//
+//  Created by Carlos Polanco on 9/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IamRich/IamRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IamRichApp.swift
+//  IamRich
+//
+//  Created by Carlos Polanco on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct IamRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
